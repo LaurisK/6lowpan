30970919c954556a9333478051d9daa75ca04ce4
Laurynas <lauris.klim@gmail.com>
2025-11-27 09:24:00 +0200

    #1784 - FEC enable

--- radio-driver.h
+++ radio-driver.h
@@ -103,7 +103,7 @@ extern "C" {
 #define VARIABLE_LENGTH             S_ENABLE
 #define EXTENDED_LENGTH_FIELD       S_DISABLE
 #define CRC_MODE                    PKT_CRC_MODE_32BITS
-#define EN_FEC                      S_DISABLE
+#define EN_FEC                      S_ENABLE
 #define EN_WHITENING                S_ENABLE
 
 #if RADIO_ADDRESS_FILTERING
